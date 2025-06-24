df9914e6a45f049b46f8cd5367225ca295a0fb48
alanrichard77 <alanrichard_@outlook.com>
2025-06-24 16:49:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def api_mispricing():
 # =====================
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
